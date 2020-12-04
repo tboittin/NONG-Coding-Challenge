3847c4cca38940229741b0e060bb6ea6410b67d4
Thomas Boittin <tboittinpro@gmail.com>
2020-12-05 00:35:40 +0400

    style dashboard in 2 parts

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import NavBar from "../NavBar";
 import NewNote from "./NewNote";
 import NotesList from "./NotesList";
-import Profilecard from "./Profilecard";
 
 export default function Dashboard() {
   return (
     <>
-      {/* <Profilecard /> */}
-      <NewNote />
-      <NotesList />
+      <NavBar />
+      <Container className="w-100" style={{ marginTop: "10vh" }}>
+        <Row className="w-100">
+          <Col xs={12} lg={{ span: 5, offset: 1 }} className="lg-mx-3">
+            <NewNote />
+          </Col>
+          <Col xs={12} lg={{ span: 5, offset: 1 }} className="lg-mx-3">
+            <NotesList />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
